ec6cd9eb48ef70bb09cb7b07a433109e7cb5d474
Mads Lundegaard <mads.lundegaard@live.no>
2021-07-08 21:15:53 +0200

    I got sicked of troubleshooting so I just imported the entire room backend

--- backend/src/main/kotlin/com/demo/security/repsitory/PasswordResetTokenRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.demo.security.repsitory
-
-import com.demo.security.token.PasswordResetToken
-import org.springframework.data.jpa.repository.JpaRepository
-import java.util.*
-
-interface PasswordResetTokenRepository : JpaRepository<PasswordResetToken, UUID>
